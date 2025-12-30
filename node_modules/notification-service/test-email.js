4578995e7ac2f2b440d552e6bb49ba978cf52bd2
yukesh <yukeshyukesh785@gmail.com>
2025-12-30 13:30:39 +0530

    Remove node_modules from git to fix Vercel deployment

--- node_modules/notification-service/test-email.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const { sendEmail } = require('./utils/emailProvider');
-
-(async () => {
-    console.log("Testing Email...");
-    const result = await sendEmail("test@example.com", "Test Subject", "<p>Test</p>");
-    console.log("Result:", result);
-})();
